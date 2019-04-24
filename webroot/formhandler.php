60692ef9e1cb871f56915a7373a9840ce638248c
James Xu <xujames007@gmail.com>
2019-04-24 18:53:24 -0400

    CI/CD

--- webroot/formhandler.php
+++ webroot/formhandler.php
@@ -1,13 +1,13 @@
 <?php
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
+//require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
-// require("./sendgrid-php.php"); 
+require("./sendgrid-php.php");
 // If not using Composer, uncomment the above line
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+//$dotenv = new Dotenv\Dotenv(__DIR__);
+//$dotenv->load();
 
 $data = array();
 $firstname = $_POST['name'];
@@ -28,7 +28,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
 
-$email = new \SendGrid\Mail\Mail(); 
+$email = new \SendGrid\Mail\Mail();
 $email->setFrom("form@codereach.ca", "Form Response");
 $email->setReplyTo($email_addr);
 $email->setSubject("Message From " . $firstname);
@@ -40,13 +40,13 @@ $email->addContent(
 $msg_file = fopen("email.txt","r");
 $msg = fread($msg_file, filesize("email.txt"));
 fclose($msg_file);
-$email2 = new \SendGrid\Mail\Mail(); 
+$email2 = new \SendGrid\Mail\Mail();
 $email2->setFrom("noreply@codereach.ca", "CodeReach Team");
 $email2->setSubject("Your contact request has been received");
 $email2->addTo($email_addr);
 $email2->addContent(
     "text/plain", "Hey " . $firstname . ",\n\nWe've received your contact request and will be responding in the next few days\n\nCodeReach Team");
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+$sendgrid = new \SendGrid($_ENV["SENDGRID_API_KEY"]);
 
 $response = $sendgrid->send($email);
 $response2 = $sendgrid->send($email2);
@@ -55,5 +55,5 @@ $response2 = $sendgrid->send($email2);
 //print $response->body() . "\n";
 
 $data['success'] = true;
-echo json_encode($data); 
+echo json_encode($data);
 ?>
